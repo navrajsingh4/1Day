4b94e961db9788f4539299c22a9248016ca54e3c
navrajsingh4 <119980499+navrajsingh4@users.noreply.github.com>
2024-04-10 12:30:25 -0600

    connection with backend, create form

--- client/src/CreateUser.jsx
+++ client/src/CreateUser.jsx
@@ -1,10 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios'
+import {useNavigate} from 'react-router-dom'
 
-function CreateUsers () {
+function CreateUsers() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [date, setDate] = useState("");
+    const [nationality, setNationality] = useState("");
+    const navigate =useNavigate()   
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios.post("http://localhost:3001/createUser",{name,email,phone,date,nationality})
+        .then(result => {
+            console.log(result)
+            navigate('/')
+            })
+        .catch(err => console.log(err))
+    };
+
     return (
         <div>
-           Create Users
+            <h2>Create Users</h2>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Name:
+                    <input 
+                        type="text" 
+                        value={name} 
+                        onChange={(e) => setName(e.target.value)} 
+                        required 
+                    />
+                </label>
+                <br />
+                <label>
+                    Email:
+                    <input 
+                        type="email" 
+                        value={email} 
+                        onChange={(e) => setEmail(e.target.value)} 
+                        required 
+                    />
+                </label>
+                <br />
+                <label>
+                    Phone:
+                    <input 
+                        type="text" 
+                        value={phone} 
+                        onChange={(e) => setPhone(e.target.value)} 
+                        required 
+                    />
+                </label>
+                <br />
+                <label>
+                    Date:
+                    <input 
+                        type="date" 
+                        value={date} 
+                        onChange={(e) => setDate(e.target.value)} 
+                        required 
+                    />
+                </label>
+                <br />
+                <br />
+                <label>
+                    Nationality:
+                    <input 
+                        type="text" 
+                        value={nationality} 
+                        onChange={(e) => setNationality(e.target.value)} 
+                        required 
+                    />
+                </label>
+                <br />
+                <button type="submit">Submit</button>
+            </form>
         </div>
-    )
+    );
 }
+
 export default CreateUsers;
---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express =require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const UserModel=require('./models/Guests')
+const app = express()
+app. use(cors())
+app.use(express.json())
+
+
+mongoose.connect("mongodb+srv://Guest:Capstone123@cluster0.imrm549.mongodb.net/DayOne")
+
+app.post("/createUser", (req, res) => {
+    UserModel.create(req.body)
+    .then(users => res.json(users))
+    .catch(err=> res.json(err))
+}
+)
+
+app.listen(3001,() => {
+    console.log("Server is Running")
+})
