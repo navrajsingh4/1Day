097943f8eb8dfa50d5c662c4fda5707625926ec8
Sha <georcia.eugenio@edu.sait.ca>
2024-04-10 18:56:12 -0600

    update

--- client/src/pages/GuestUserManagement.js
+++ client/src/pages/GuestUserManagement.js
@@ -1,48 +1,27 @@
-import React, { useState } from 'react';
-import { Routes, Route, Link, BrowserRouter } from 'react-router-dom';
-import CreateUser from './components/CreateUser';
-import UpdateUser from './components/UpdateUser';
-import DeleteUser from './components/DeleteUser';
+// GuestUserManagement.js
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import CreateUsers from './CreateUsers'; // Import the CreateUsers component
 
 function GuestUserManagement() {
-  const [activeTab, setActiveTab] = useState('create');
-
   return (
-    <BrowserRouter>
-      <div>
-        <nav>
-          <ul>
-            <li onClick={() => setActiveTab('create')}>
-              <Link to="/guest-management/create">Create Guest User</Link>
-            </li>
-            <li onClick={() => setActiveTab('update')}>
-              <Link to="/guest-management/update">Update Guest User</Link>
-            </li>
-            <li onClick={() => setActiveTab('delete')}>
-              <Link to="/guest-management/delete">Delete Guest User</Link>
-            </li>
-          </ul>
-        </nav>
+    <div>
+      {/* Navigation links can be used to switch between different management tasks */}
+      <nav>
+        <ul>
+          <li>
+            <Link to="create">Create Guest User</Link>
+          </li>
+          {/* Add links for update and delete when those components are created */}
+        </ul>
+      </nav>
 
-        <div className={activeTab === 'create' ? 'active' : ''}>
-          <Routes>
-            <Route path="/guest-management/create" element={<CreateUser />} />
-          </Routes>
-        </div>
-
-        <div className={activeTab === 'update' ? 'active' : ''}>
-          <Routes>
-            <Route path="/guest-management/update" element={<UpdateUser />} />
-          </Routes>
-        </div>
-
-        <div className={activeTab === 'delete' ? 'active' : ''}>
-          <Routes>
-            <Route path="/guest-management/delete" element={<DeleteUser />} />
-          </Routes>
-        </div>
-      </div>
-    </BrowserRouter>
+      {/* The Routes for managing guest users */}
+      <Routes>
+        <Route path="create" element={<CreateUsers />} />
+        {/* Add routes for update and delete when those components are created */}
+      </Routes>
+    </div>
   );
 }
 
