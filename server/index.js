4a925ea2dcba5ef80e95be6b620e94595c008961
Sha <georcia.eugenio@edu.sait.ca>
2024-04-10 18:40:59 -0600

    update

--- server/index.js
+++ server/index.js
@@ -30,7 +30,44 @@ app.get('/getUser/',(req, res) =>{
     .then(users => res.json(users))
     .catch(err=> res.json(err))
 })
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log('error', err));
 
-app.listen(3001,() => {
-    console.log("Server is Running")
-})
+// get users
+app.get("/", async (req, res) => {
+    try {
+        const users = await UserModel.find({}).exec();
+        res.json(users);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+// Create user
+app.post("/createUser", async (req, res) => {
+    try {
+        const user = await UserModel.create(req.body);
+        res.json(user);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+// Retrieve user by ID 
+app.get('/getUser/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const user = await UserModel.findById(id).exec(); // Use exec here as well
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+        res.json(user);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+// Start
+app.listen(3001, () => {
+    console.log("server running");
+});
