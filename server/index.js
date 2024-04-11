90d84d9c994ebf39a01771eedbbedbdb085e692c
Sha <georcia.eugenio@edu.sait.ca>
2024-04-10 19:26:55 -0600

    fix

--- server/index.js
+++ server/index.js
@@ -1,36 +1,55 @@
-const express =require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const UserModel=require('./models/Guests')
-const app = express()
-app. use(cors())
-app.use(express.json())
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const UserModel = require('./models/Guests');
+const app = express();
 
+app.use(cors());
+app.use(express.json());
 
-mongoose.connect("mongodb+srv://Guest:Capstone123@cluster0.imrm549.mongodb.net/DayOne")
-
-app.get("/", async (req,res) => {
-    UserModel.find({})
-    .then(users => res.json(users))
-    .then(err => res.json(err))
-}
-
-)
-
-app.post("/createUser", (req, res) => {
-    UserModel.create(req.body)
-    .then(users => res.json(users))
-    .catch(err=> res.json(err))
-}
-)
-
-app.get('/getUser/',(req, res) =>{
-    const id = req.params.id;
-    UserModel.findById({_id:id})
-    .then(users => res.json(users))
-    .catch(err=> res.json(err))
+// Connect to MongoDB
+mongoose.connect("mongodb+srv://Guest:Capstone123@cluster0.imrm549.mongodb.net/DayOne", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log('error', err));
 
-app.listen(3001,() => {
-    console.log("Server is Running")
-})
+// get users
+app.get("/", async (req, res) => {
+    try {
+        const users = await UserModel.find({}).exec();
+        res.json(users);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+// Create user
+app.post("/createUser", async (req, res) => {
+    try {
+        const user = await UserModel.create(req.body);
+        res.json(user);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+// Retrieve user by ID 
+app.get('/getUser/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const user = await UserModel.findById(id).exec(); // Use exec here as well
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+        res.json(user);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+// Start
+app.listen(3001, () => {
+    console.log("server running");
+});
